d364ca7bd1299e4b36d935c296455b0f3d2c9b41
aGith0bszkola <githubszkola@gmail.com>
2026-09-24 10:38:30 +0200

    layout i R.id

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +13,22 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+Button buttonNastepne;
+RadioButton radioButtonA,RadioButtonB, RadioButtonC;
+RadioGroup radioGroupPytania;
+TextView textViewTresc;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+            buttonNastepne = findViewById(R.id.button);
+            radioButtonA = findViewById(R.id.radioButton);
+            RadioButtonB = findViewById(R.id.radioButton2);
+            RadioButtonC = findViewById(R.id.radioButton3);
+            textViewTresc = findViewById(R.id.textViewPytanie);
+            radioGroupPytania = findViewById(R.id.RadioGrupa);
+
     }
+
 }
